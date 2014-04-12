d78e0adb2dbd67e8dceaac944cda3e0538788681
Paul B <tpb.api@gmail.com>
2014-04-13 00:08:39 +0200

    Update main urls for this django app and serve the torrents api

--- wsgi/api/urls.py
+++ wsgi/api/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
-
 from django.conf import settings
+from torrents import urls
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'views.home', name='home'),
+    url(r'^torrents/', include(urls)),
     # url(r'^$', 'openshift.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     
-    url(r'^admin/', include(admin.site.urls)),
+    # url(r'^admin/', include(admin.site.urls)),
 )
